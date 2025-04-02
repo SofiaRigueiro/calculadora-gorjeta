ba3217575025d3e197b6c27cf498891b06e62edc
sofia rigueiro <sofiarigueiro@eaportal.org>
2025-04-02 00:04:18 -0300

    criação da lógica de selecionar um botão de gorjeta ou colocar um valor customizado

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
 let bill = 0
-let tipPercentage = 0
-let numberOfPeople = 0
-let buttonSelected = null
-
-function receiveBillValue() {
-    bill = document.querySelector("#bill").valueAsNumber
-}
-
-function receiveNumberOfPeopleValue() {
-    numberOfPeople = document.querySelector("#people").valueAsNumber
-}
-
-function receiveTipPercentageValue(value) {
-    tipPercentage = value / 100
-
-    if (buttonSelected !== null){
-        buttonSelected.classList.remove("button-selected")
-    }
-
-    buttonSelected = document.querySelector(`#button-${value}`)
-    buttonSelected.classList.add("button-selected")
-}
+ let tipPercentage = 0
+ let numberOfPeople = 0
+ let buttonSelected = null
+ 
+ function receiveBillValue() {
+     bill = document.querySelector("#bill").valueAsNumber
+ }
+ 
+ function receiveNumberOfPeopleValue() {
+     numberOfPeople = document.querySelector("#people").valueAsNumber
+ }
+ 
+ function receiveTipPercentageValue(value) {
+     tipPercentage = value / 100
+ 
+     document.querySelector("#custom-tip").value = ""
+ 
+     if (buttonSelected !== null){
+         buttonSelected.classList.remove("button-selected")
+     }
+ 
+     buttonSelected = document.querySelector(`#button-${value}`)
+     buttonSelected.classList.add("button-selected")
+ }
+ 
+ function receiveCustomTipPercentageValue () {
+     tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
+ 
+     removeClassButtonSelected()
+ }
+ 
+ function removeClassButtonSelected() {
+     if(buttonSelected !== null){
+         buttonSelected.classList.remove("button-selected")
+         buttonSelected = null
+     }
+ }
